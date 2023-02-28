c1afe54374ff48c2344168d4d4e9fc2d36a24f9a
Eunice Sarah Siregar <13521013@mahasiswa.itb.ac.id>
2023-02-28 20:05:27 +0700

    selesai yey

--- function.py
+++ function.py
@@ -1,7 +1,18 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 import math as m
+import random
+import os
+
+os.system('cls')
+print(" ██████╗██╗      ██████╗ ███████╗███████╗███████╗████████╗██████╗  █████╗ ██╗██████╗ ")
+print("██╔════╝██║     ██╔═══██╗██╔════╝██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║██╔══██╗")
+print("██║     ██║     ██║   ██║███████╗█████╗  ███████╗   ██║   ██████╔╝███████║██║██████╔╝")
+print("██║     ██║     ██║   ██║╚════██║██╔══╝  ╚════██║   ██║   ██╔═══╝ ██╔══██║██║██╔══██╗")
+print("╚██████╗███████╗╚██████╔╝███████║███████╗███████║   ██║   ██║     ██║  ██║██║██║  ██║")
+print(" ╚═════╝╚══════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═╝")
+print("=====================================================================================")
 dot = int(input("Masukkan jumlah titik: "))
 dim = int(input("Masukkan jumlah dimensi: "))
 
@@ -10,15 +21,10 @@ def random_matrix(dot, dim):
     # print(matrix)
     for i in range(dim):
         for j in range(dot):
-            matrix[j][i] = np.random.randint(0, 100)
-    print(matrix)
+            matrix[j][i] = random.randint(0, 100)
+    # print(matrix)
     return matrix
 
-# def scatter_plot(matrix):
-#     fig = plt.figure(figsize=(4,4))
-#     ax = fig.add_subplot(111, projection='3d')
-#     ax.scatter(matrix[:, 0], matrix[:, 1], matrix[:, 2])
-#     plt.show()
 
 def distance_matrix(matrix):
     dist_matrix = [[0 for i in range(dim)] for j in range(dim)]
@@ -27,14 +33,15 @@ def distance_matrix(matrix):
     for i in range(dim):
         for j in range(dim):
             dist_matrix[j][i] = (matrix[0][i] - matrix[1][i])**2
+            
             # print(i, j)
         # print(dist_matrix[j][i])
         sum += dist_matrix[j][i]
     distance = m.sqrt(sum)
-    print(distance)
-    return distance
+    # print(distance)
+    return distance, matrix[0], matrix[1]
+# distance_matrix(a)
 a = random_matrix(dot, dim)
-distance_matrix(a)
 
 def sortX(matrix):
     for i in range(len(matrix)):
